7519000173d2126c7443e71328ebb34669735605
IMacCasa <jhernandez@iMac-de-IMacCasa.local>
2024-07-13 00:02:27 -0400

    coffe POS adjustments

---
+++ src/app/coffe/orders/OrderManager.jsx
@@ -0,0 +1,139 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { GetFireBaseDataAll } from "@/Components/Firebase/DataManager/DataOperations";
+import OrdersCard from "./orderCards/orderCards";
+import "./styles.css";
+import { UpdateRecord } from "@/Components/Firebase/DataManager/DataOperations";
+
+const OrderManagerPage = ({ orderDetails, setOrderDetails }) => {
+  // console.log("detalle de la orden", orderDetails);
+
+  const [orders, setOrders] = useState(null);
+  const [loading, setLoading] = useState(true); // Initialize loading state
+  const [mainOrderData, setMainOrderData] = useState(null);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const DBEvento = "Coffe"; // Replace with the actual collection name
+        const data = await GetFireBaseDataAll(DBEvento);
+        setOrders(data);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      } finally {
+        setLoading(false); // Update loading state
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  const HandleDeliveredStatus = () => {
+    console.log();
+    UpdateRecord('Coffe',mainOrderData?.id,'','Jonathan');
+  };
+
+  return (
+    <>
+      {/* <h1>Administrador de ventas Coffe</h1> */}
+      <section className="coffe-manager-body-container">
+        <section className="coffe-manager-body-products-navigation">
+          <section className="coffe-manager-body-menu-container">
+            <div className="menu-filter orders-filter">
+              <div>Todos los pedidos</div>
+              <div>Pedidos Nuevos</div>
+              <div>Pedidos en Proceso</div>
+              <div>Pedidos Completados</div>
+            </div>
+            <div className="orders-container">
+              {loading ? (
+                <div>Loading...</div> // You can replace this with a loading spinner or any other loading indicator
+              ) : orders.length > 0 ? (
+                orders.map((order, index) => (
+                  <OrdersCard
+                    key={index}
+                    orders={order}
+                    setOrderDetails={setOrderDetails}
+                    setMainOrderData={setMainOrderData}
+                  />
+                ))
+              ) : (
+                <div>No orders available</div> // Message when there are no orders
+              )}
+            </div>
+          </section>
+        </section>
+
+        <div className="coffe-manager-body-order-details-container">
+          <div className="order-details-header">
+            {/* <div className="text">
+          <p>Detalle de la orden</p>
+        </div> */}
+            <div className="details">
+              <div>
+                <div>
+                  <p className="orderFinder">Orden:</p>
+                  {mainOrderData?.id}
+                </div>
+                <p>Hora Ingreso: {mainOrderData?.id}</p>
+                {/* <p>Estado: {mainOrderData?.orderStatus}</p> */}
+                <p>Estado: </p>
+              </div>
+            </div>
+            <div>
+              <div className="details-values"></div>
+            </div>
+          </div>
+          <div className="products-invoice-details">
+            <div className="orders-list">
+              <table>
+                <thead>
+                  <tr>
+                    <td>Img</td>
+                    <td>Producto</td>
+                    <td>Preparado?</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orderDetails && orderDetails.length > 0 ? (
+                    orderDetails.map((details, index) => (
+                      <tr className="tr-container" key={index}>
+                        <td>
+                          <img
+                            className="td-image"
+                            src={details.Product_Img}
+                            alt={details.Product_Name}
+                          />
+                        </td>
+                        <td>
+                          <p>{details.Product_Name}</p>
+                        </td>
+                        <td>
+                          <input
+                            type="checkbox"
+                            id={details.Product_index}
+                            checked={details.Product_Status === "Ready"}
+                            onClick={() => HandleDeliveredStatus()}
+                          />
+                          {/* <label htmlFor={index}> Entregado</label> */}
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="3">No order details available</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="order-details-footer"></div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default OrderManagerPage;
--- src/app/coffe/orders/orderCards/orderCards.jsx
+++ src/app/coffe/orders/orderCards/orderCards.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import "./styles.css";
 
-const OrdersCard = ({ orders }) => {
+const OrdersCard = ({ orders, setOrderDetails, setMainOrderData }) => {
   const {
     id,
     orderCustomerName,
@@ -11,37 +11,32 @@ const OrdersCard = ({ orders }) => {
     orderDetails,
   } = orders;
 
-  // console.log(orderDetails);
+  // console.log("Detalle del Pedido", orders.orderDetails);
+
+  const HandleOrderCheck = (details) => {
+    setOrderDetails(details);
+    setMainOrderData(orders);
+  };
 
   return (
     <>
       {/* <div className={`products-card ${!newOrder ? "disabled" : ""}`}> */}
-      <div className="orderCard-container">
+      <div
+        className="orderCard-container"
+        onClick={() => HandleOrderCheck(orders.orderDetails)}
+      >
         <section className="order-header">
-          <h1>ID de la orden :{id}</h1>
-          <h1>Cliente:{orderCustomerName}</h1>
+          <h1>Orden :{id}</h1>
+          <h1>Cliente: {orderCustomerName}</h1>
         </section>
         <section className="order-details">
-          <p>Detalle del pedido:</p>
-          <div className="orders-list">
-            {/* Crear tabla para visualizar mejor el producto */}
-            {orderDetails?.map((list, index) => {
-              return (
-                <>
-                  <img src={list.Product_Img} alt="" />
-                  <div>
-                    {list.Product_Name}
-                    <input type="checkbox" name="" id="" />
-                  </div>
-                </>
-              );
-            })}
-          </div>
-          {/* <p className="product-details">{product.Product_Desc}</p> */}
-          {/* <p className="product-price">$ {product.Product_Price}</p> */}
+          <p>Detalle del pedido</p>
         </section>
         <section className="order-footer">
-          <p>Estado de la orden 🟢🟡</p>
+          <progress className="progress-bar" value="50" max="100"></progress>
+
+          {/* <p>Estado de la orden 🟢 🟡 🔴</p>
+          <button type="button">Pedido Completado</button> */}
           {/* <p>{orderStatus}</p> */}
         </section>
       </div>
